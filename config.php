70886f36de558ccb9028ddb20639b26a595b3775
ecleese <ecleese@gmail.com>
2013-10-10 22:14:49 -0400

    Update config.php

--- config.php
+++ config.php
@@ -4,8 +4,8 @@
 //	plexWatchWeb config file
 
 
-$plexWatch['pmsUrl'] = '192.168.0.50';							// Plex Media Server IP or hostname
-$plexWatch['pmsPort'] = '32400';								// Plex Media Server port
+$plexWatch['pmsUrl'] = '192.168.0.50';					              // Plex Media Server IP or hostname
+$plexWatch['pmsPort'] = '32400';								              // Plex Media Server port
 $plexWatch['plexWatchDb'] = '/opt/plexWatch/plexWatch.db';		// Location of your plexWatch database
 
 
