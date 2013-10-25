ee1fc3bd80b1108e7f14c2c1d030de4493390bf5
ljunkie <github@linuxjunkie.com>
2013-10-25 16:42:07 -0700

    hide the password

--- settings.php
+++ settings.php
@@ -170,7 +170,7 @@
 					<div class="control-group">
 					  <label class="control-label" for="myPlexPass">myPlex Password (optional)</label>
 					  <div class="controls">
-						  <input id="myPlexPass" name="myPlexPass" type="text" placeholder="" class="input-xlarge" value="<?php echo $plexWatch['myPlexPass'] ?>">
+						  <input id="myPlexPass" name="myPlexPass" type="password" placeholder="" class="input-xlarge" value="<?php echo $plexWatch['myPlexPass'] ?>">
 						  <p class="help-block">If you would like to access plexWatch/Web on other networks, a myPlex username and password are required.</p>
 					  </div>
 					</div>
