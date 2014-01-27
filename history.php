dd469ca0262ef602f20ab49c4fea59a1ee1eeb6b
Andy Cook <andy@cookandy.com>
2014-01-27 23:22:38 +0000

    Revert "v1.5.0.16 dev"
    This reverts commit 129f04b8f752941d93bf3993cdb6d65cf5a60f3f.

--- history.php
+++ history.php
@@ -204,18 +204,11 @@
 							$from_time = strtotime(date("m/d/Y g:i a",$row['time']));
 							$paused_time = strtotime(date("m/d/Y g:i a",$row['paused_counter']));
 							
-							
-							if (empty($row['stopped'])) {								
-								$stopped_time = '<span class="currentlyWatching " align="center">Currently watching...</span>';
-							}else{
-								$stopped_time = $stopped_time;
-							}
-							
 							$viewed_time = round(abs($to_time - $from_time - $paused_time) / 60,0);
 							$viewed_time_length = strlen($viewed_time);
 							
 							if ($viewed_time_length == 8) {
-								$viewed_time_length = '<span class="currentlyWatching " align="center">Currently watching...</span>';
+								$viewed_time_length = 'Not Finished';
 							}else{
 								$viewed_time_length = $viewed_time_length.' min';
 							}
@@ -285,8 +278,8 @@
 											<br/><h4>Time Information</h4>
 											<ul>
 											<li>Start Time: <strong><?php echo date($plexWatch['timeFormat'],$row['time']); ?></strong></li>
-											<li>Minutes Paused: <strong><?php echo $paused_duration.' min'; ?></strong></li>
 											<li>Stop Time: <strong><?php echo $stopped_time; ?></strong></li>
+											<li>Minutes Paused: <strong><?php echo $paused_duration.' min'; ?></strong></li>
 											<li>Duration: <strong><?php echo $viewed_time_length; ?></strong></li>
 											</ul>
 										</div>
@@ -349,8 +342,8 @@
 											<br/><h4>Time Information</h4>
 											<ul>
 											<li>Start Time: <strong><?php echo date($plexWatch['timeFormat'],$row['time']); ?></strong></li>
-											<li>Minutes Paused: <strong><?php echo $paused_duration.' min'; ?></strong></li>
 											<li>Stop Time: <strong><?php echo $stopped_time; ?></strong></li>
+											<li>Minutes Paused: <strong><?php echo $paused_duration.' min'; ?></strong></li>
 											<li>Duration: <strong><?php echo $viewed_time_length; ?></strong></li>
 											</ul>
 										</div>
