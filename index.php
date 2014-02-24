8935dc4cac3a07b385e509af920d3cd6bde22ae1
phawkins <paul.hawkins@californiasteel.com>
2014-02-24 12:16:18 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php include("../login/include/session.php"); ?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -38,14 +39,14 @@
 			<div class="navbar-inner">
 				<a href="index.php"><div class="logo hidden-phone"></div></a>
 				<ul class="nav">
-					
+					<?php if($session->logged_in){ ?>
 					<li class="active"><a href="index.php"><i class="icon-2x icon-home icon-white" data-toggle="tooltip" data-placement="bottom" title="Home" id="home"></i></a></li>
 					<li><a href="history.php"><i class="icon-2x icon-calendar icon-white" data-toggle="tooltip" data-placement="bottom" title="History" id="history"></i></a></li>
 					<li><a href="stats.php"><i class="icon-2x icon-tasks icon-white" data-toggle="tooltip" data-placement="bottom" title="Stats" id="stats"></i></a></li>
 					<li><a href="users.php"><i class="icon-2x icon-group icon-white" data-toggle="tooltip" data-placement="bottom" title="Users" id="users"></i></a></li>
 					<li><a href="charts.php"><i class="icon-2x icon-bar-chart icon-white" data-toggle="tooltip" data-placement="bottom" title="Charts" id="charts"></i></a></li>
 					<li><a href="settings.php"><i class="icon-2x icon-wrench icon-white" data-toggle="tooltip" data-placement="bottom" title="Settings" id="settings"></i></a></li>
-					
+					<?php } ?>
 				</ul>
 				
 			</div>
@@ -267,6 +268,6 @@
 	});
 	</script>
 
-
+<?php if($session->logged_in){ ?><a href="../login/process.php">Logout</a><?php } else { ?> <a href="../login/main.php">Login</a> <?php } ?>
   </body>
 </html>
