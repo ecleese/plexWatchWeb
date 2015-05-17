58f97e0f9b6fc2ee0e601c81eb3f3b2fb397a996
Landon Abney <Landonabney@gmail.com>
2015-05-16 20:03:06 -0700

    Clean up datafactory output
    Remove all the extraneous whitespace that was being output from the
    get-*-info-model pages. Also moves to using the changed dbTable().
    
    These two files do the exact same thing, should probably be merged and
    the caller(s) changed to accommodate that.

--- datafactory/get-user-info-modal.php
+++ datafactory/get-user-info-modal.php
@@ -2,7 +2,7 @@
 date_default_timezone_set(@date_default_timezone_get());
 
 $guisettingsFile = "../config/config.php";
-if (file_exists($guisettingsFile)) { 
+if (file_exists($guisettingsFile)) {
 	require_once('../config/config.php');
 } else {
 	error_log('PlexWatchWeb :: Config file not found.');
@@ -20,110 +20,75 @@ if (isset($_POST['id'])) {
 	exit;
 }
 
-$plexWatchDbTable = "";
-
-if ($plexWatch['userHistoryGrouping'] == "yes") {
-	$plexWatchDbTable = "grouped";
-} else if ($plexWatch['userHistoryGrouping'] == "no") {
-	$plexWatchDbTable = "processed";
-}
+$plexWatchDbTable = dbTable('user'); // Grabs user setting
 $results = $db->querySingle("SELECT xml FROM $plexWatchDbTable WHERE id = $id") or die ("Failed to access plexWatch database. Please check your settings.");
-$xmlfield = simplexml_load_string($results); 
+$xmlfield = simplexml_load_string($results);
 
-if (array_key_exists('TranscodeSession',$xmlfield)) { ?>
-	<div class="span4">
-		<h4>Stream Details</h4>
-		<ul>
-			<h5>Video</h5>
-			<li>Stream Type: <strong><?php echo $xmlfield->TranscodeSession['videoDecision']; ?></strong></li>
-			<li>Video Resolution: <strong><?php echo $xmlfield->TranscodeSession['height']; ?>p</strong></li>
-			<li>Video Codec: <strong><?php echo $xmlfield->TranscodeSession['videoCodec']; ?></strong></li>
-			<li>Video Width: <strong><?php echo $xmlfield->TranscodeSession['width']; ?></strong></li>
-			<li>Video Height: <strong><?php echo $xmlfield->TranscodeSession['height']; ?></strong></li>
-		</ul>
-		<ul>
-			<h5>Audio</h5>
-			<li>Stream Type: <strong><?php echo $xmlfield->TranscodeSession['audioDecision']; ?></strong></li>
-			<?php if ($xmlfield->TranscodeSession['audioCodec'] == "dca") { ?>
-				<li>Audio Codec: <strong>dts</strong></li>
-			<?php } else { ?>
-				<li>Audio Codec: <strong><?php echo $xmlfield->TranscodeSession['audioCodec']; ?></strong></li>
-			<?php } ?>
-			<li>Audio Channels: <strong><?php echo $xmlfield->TranscodeSession['audioChannels']; ?></strong></li>
-		</ul>
-	</div>
-	<div class="span4">
-		<h4>Media Source Details</h4>
-		<li>Container: <strong><?php echo $xmlfield->Media['container']; ?></strong></li>
-		<li>Resolution: <strong><?php echo $xmlfield->Media['videoResolution']; ?>p</strong></li>
-		<li>Bitrate: <strong><?php echo $xmlfield->Media['bitrate']; ?> kbps</strong></li>
-	</div>
-	<div class="span4">	
-		<h4>Video Source Details</h4>
-		<ul>
-			<li>Width: <strong><?php echo $xmlfield->Media['width']; ?></strong></li>
-			<li>Height: <strong><?php echo $xmlfield->Media['height']; ?></strong></li>
-			<li>Aspect Ratio: <strong><?php echo $xmlfield->Media['aspectRatio']; ?></strong></li>											
-			<li>Video Frame Rate: <strong><?php echo $xmlfield->Media['videoFrameRate']; ?></strong></li>
-			<li>Video Codec: <strong><?php echo $xmlfield->Media['videoCodec']; ?></strong></li>
-		</ul>
-		<ul></ul>
-		<h4>Audio Source Details</h4>
-		<ul>
-			<?php if ($xmlfield->Media['audioCodec'] == "dca") { ?>
-				<li>Audio Codec: <strong>dts</strong></li>
-			<?php } else { ?>
-				<li>Audio Codec: <strong><?php echo $xmlfield->Media['audioCodec']; ?></strong></li>
-			<?php } ?>
-			<li>Audio Channels: <strong><?php echo $xmlfield->Media['audioChannels']; ?></strong></li>
-		</ul>
-	</div>
-<?php } else { ?>
-	<div class="span4">
-		<h4>Stream Details</strong></h4>
-		<ul>
-			<h5>Video</h5>
-			<li>Stream Type: <strong>Direct Play</strong></li>
-			<li>Video Resolution: <strong><?php echo $xmlfield->Media['videoResolution']; ?>p</strong></li>
-			<li>Video Codec: <strong><?php echo $xmlfield->Media['videoCodec']; ?></strong></li>
-			<li>Video Width: <strong><?php echo $xmlfield->Media['width']; ?></strong></li>
-			<li>Video Height: <strong><?php echo $xmlfield->Media['height']; ?></strong></li>
-		</ul>
-		<ul>
-			<h5>Audio</h5>
-			<li>Stream Type: <strong>Direct Play</strong></li>
-			<?php if ($xmlfield->Media['audioCodec'] == "dca") { ?>
-				<li>Audio Codec: <strong>dts</strong></li>
-			<?php } else { ?>
-				<li>Audio Codec: <strong><?php echo $xmlfield->Media['audioCodec']; ?></strong></li>
-			<?php } ?>
-				<li>Audio Channels: <strong><?php echo $xmlfield->Media['audioChannels']; ?></strong></li>
-		</ul>
-	</div>
-	<div class="span4">
-		<h4>Media Source Details</h4>
-		<li>Container: <strong><?php echo $xmlfield->Media['container']; ?></strong></li>
-		<li>Resolution: <strong><?php echo $xmlfield->Media['videoResolution']; ?>p</strong></li>
-		<li>Bitrate: <strong><?php echo $xmlfield->Media['bitrate']; ?> kbps</strong></li>
-	</div>
-	<div class="span4">	
-		<h4>Video Source Details</h4>
-		<ul>
-			<li>Width: <strong><?php echo $xmlfield->Media['width']; ?></strong></li>
-			<li>Height: <strong><?php echo $xmlfield->Media['height']; ?></strong></li>
-			<li>Aspect Ratio: <strong><?php echo $xmlfield->Media['aspectRatio']; ?></strong></li>											
-			<li>Video Frame Rate: <strong><?php echo $xmlfield->Media['videoFrameRate']; ?></strong></li>
-			<li>Video Codec: <strong><?php echo $xmlfield->Media['videoCodec']; ?></strong></li>
-		</ul>
-		<ul></ul>
-		<h4>Audio Source Details</h4>
-		<ul>
-			<?php if ($xmlfield->Media['audioCodec'] == "dca") { ?>
-				<li>Audio Codec: <strong>dts</strong></li>
-			<?php } else { ?>
-				<li>Audio Codec: <strong><?php echo $xmlfield->Media['audioCodec']; ?></strong></li>
-			<?php } ?>
-			<li>Audio Channels: <strong><?php echo $xmlfield->Media['audioChannels']; ?></strong></li>
-		</ul>
-	</div>
-<?php } ?>
+$transcoded = array_key_exists('TranscodeSession', $xmlfield);
+
+echo '<div class="span4">';
+	echo '<h4>Stream Details</h4>';
+		echo '<ul>';
+			echo '<h5>Video</h5>';
+			if ($transcoded) {
+				echo '<li>Stream Type: <strong>'.$xmlfield->TranscodeSession['videoDecision'].'</strong></li>';
+				echo '<li>Video Resolution: <strong>'.$xmlfield->TranscodeSession['height'].'p</strong></li>';
+				echo '<li>Video Codec: <strong>'.$xmlfield->TranscodeSession['videoCodec'].'</strong></li>';
+				echo '<li>Video Width: <strong>'.$xmlfield->TranscodeSession['width'].'</strong></li>';
+				echo '<li>Video Height: <strong>'.$xmlfield->TranscodeSession['height'].'</strong></li>';
+			} else {
+				echo '<li>Stream Type: <strong>Direct Play</strong></li>';
+				echo '<li>Video Resolution: <strong>'.$xmlfield->Media['videoResolution'].'p</strong></li>';
+				echo '<li>Video Codec: <strong>'.$xmlfield->Media['videoCodec'].'</strong></li>';
+				echo '<li>Video Width: <strong>'.$xmlfield->Media['width'].'</strong></li>';
+				echo '<li>Video Height: <strong>'.$xmlfield->Media['height'].'</strong></li>';
+			}
+		echo '</ul>';
+		echo '<ul>';
+			echo '<h5>Audio</h5>';
+			if ($transcoded) {
+				echo '<li>Stream Type: <strong>'.$xmlfield->TranscodeSession['audioDecision'].'</strong></li>';
+				if ($xmlfield->TranscodeSession['audioCodec'] == "dca") {
+					echo '<li>Audio Codec: <strong>dts</strong></li>';
+				} else {
+					echo '<li>Audio Codec: <strong>'.$xmlfield->TranscodeSession['audioCodec'].'</strong></li>';
+				}
+				echo '<li>Audio Channels: <strong>'.$xmlfield->TranscodeSession['audioChannels'].'</strong></li>';
+			} else {
+				echo '<li>Stream Type: <strong>Direct Play</strong></li>';
+				if ($xmlfield->Media['audioCodec'] == "dca") {
+					echo '<li>Audio Codec: <strong>dts</strong></li>';
+				} else {
+					echo '<li>Audio Codec: <strong>'.$xmlfield->Media['audioCodec'].'</strong></li>';
+				}
+				echo '<li>Audio Channels: <strong>'.$xmlfield->Media['audioChannels'].'</strong></li>';
+			}
+		echo '</ul>';
+echo '</div>';
+echo '<div class="span4">';
+	echo '<h4>Media Source Details</h4>';
+	echo '<li>Container: <strong>'.$xmlfield->Media['container'].'</strong></li>';
+	echo '<li>Resolution: <strong>'.$xmlfield->Media['videoResolution'].'p</strong></li>';
+	echo '<li>Bitrate: <strong>'.$xmlfield->Media['bitrate'].' kbps</strong></li>';
+echo '</div>';
+echo '<div class="span4">';
+	echo '<h4>Video Source Details</h4>';
+	echo '<ul>';
+		echo '<li>Width: <strong>'.$xmlfield->Media['width'].'</strong></li>';
+		echo '<li>Height: <strong>'.$xmlfield->Media['height'].'</strong></li>';
+		echo '<li>Aspect Ratio: <strong>'.$xmlfield->Media['aspectRatio'].'</strong></li>';
+		echo '<li>Video Frame Rate: <strong>'.$xmlfield->Media['videoFrameRate'].'</strong></li>';
+		echo '<li>Video Codec: <strong>'.$xmlfield->Media['videoCodec'].'</strong></li>';
+	echo '</ul>';
+	echo '<ul></ul>';
+	echo '<h4>Audio Source Details</h4>';
+	echo '<ul>';
+		if ($xmlfield->Media['audioCodec'] == "dca") {
+			echo '<li>Audio Codec: <strong>dts</strong></li>';
+		} else {
+			echo '<li>Audio Codec: <strong>'.$xmlfield->Media['audioCodec'].'</strong></li>';
+		}
+		echo '<li>Audio Channels: <strong>'.$xmlfield->Media['audioChannels'].'</strong></li>';
+	echo '</ul>';
+echo '</div>';
+?>
