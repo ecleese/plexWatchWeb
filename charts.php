81123ede016cd2fbccf32b6a8ccd783b2332e011
Landon Abney <Landonabney@gmail.com>
2015-06-08 14:32:00 -0700

    Minor cleanup of charts
    Found some code that did nothing that could be removed, and noticed that
    one of the columns being pulled in from the DB was unused.

--- charts.php
+++ charts.php
@@ -9,18 +9,9 @@ if (file_exists($guisettingsFile)) {
 	return;
 }
 
-if ($fileContents = getPMSData('/status/sessions')) {
-	$statusSessions = simplexml_load_string($fileContents);
-	if ($statusSessions === false) {
-		$error_msg = 'Failed to access Plex Media Server. Please check your settings.';
-		echo '<p>' . $error_msg . '</p>';
-		trigger_error($error_msg, E_USER_ERROR);
-	}
-}
-
 $database = dbconnect();
 $plexWatchDbTable = dbTable('charts');
-$columns = "title,time,orig_title,orig_title_ep,episode," .
+$columns = "title,time,orig_title,episode," .
 	"season,xml,COUNT(*) AS play_count ";
 
 function printTop10($query, $type = null) {
@@ -53,7 +44,6 @@ function printTop10($query, $type = null) {
 					break;
 			}
 		}
-
 		echo '<div class="charts-instance-wrapper">';
 			echo '<div class="charts-instance-position-circle">';
 				echo '<h1>' . $num_rows . '</h1>';
