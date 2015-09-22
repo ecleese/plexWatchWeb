d0f1c2e27a7d5fb0bfff7a89c51fd2bdd4a98395
cookandy <andycook@Andys-MacBook-Pro.local>
2015-09-21 20:57:18 -0700

    created header file
    created header.php and referenced it from all front-end pages

--- users.php
+++ users.php
@@ -34,21 +34,8 @@ require_once(dirname(__FILE__) . '/includes/timeago.php');
 		<link rel="apple-touch-icon" sizes="144x144" href="images/icon_ipad@2x.png">
 	</head>
 	<body>
-		<div class="container">
-			<div class="navbar navbar-fixed-top">
-				<div class="navbar-inner">
-					<a href="index.php"><div class="logo hidden-phone"></div></a>
-					<ul class="nav">
-						<li><a href="index.php"><i class="fa fa-home fa-2x" data-toggle="tooltip" data-placement="bottom" title="Home" id="home"></i></a></li>
-						<li><a href="history.php"><i class="fa fa-history fa-2x" data-toggle="tooltip" data-placement="bottom" title="History" id="history"></i></a></li>
-						<li class="active"><a href="users.php"><i class="fa fa-users fa-2x" data-toggle="tooltip" data-placement="bottom" title="Users" id="users"></i></a></li>
-						<li><a href="stats.php"><i class="fa fa-area-chart fa-2x" data-toggle="tooltip" data-placement="bottom" title="Stats" id="stats"></i></a></li>
-						<li><a href="charts.php"><i class="fa fa-bar-chart fa-2x" data-toggle="tooltip" data-placement="bottom" title="Charts" id="charts"></i></a></li>
-						<li><a href="settings.php"><i class="fa fa-cogs fa-2x" data-toggle="tooltip" data-placement="bottom" title="Settings" id="settings"></i></a></li>
-					</ul>
-				</div>
-			</div>
-		</div>
+
+	<?php include 'header.php' ?>
 
 		<div class="container-fluid">
 			<div class="row-fluid">
